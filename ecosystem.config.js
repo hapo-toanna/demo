44183a70e22b0319548d142fe9e9cd2648bafa77
hapo-toanna <toan.na@haposoft.com>
2022-11-25 16:36:28 +0700

    test2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,9 +11,9 @@ module.exports = {
     production : {
       user : 'ubuntu',
       host : '192.168.1.105',
-      ref  : 'origin/master',
+      ref  : 'origin/main',
       repo : 'git@github.com:hapo-toanna/demo.git',
-      path : '/code/test/demo',
+      path : '/home/ubuntu',
       'pre-deploy-local': '',
       'post-deploy' : 'source ~/.nvm/nvm.sh npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
