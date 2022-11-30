fde35f2dba551fc15a6a4ec79e8318c56a5d9988
hapo-toanna <toan.na@haposoft.com>
2022-11-30 11:01:43 +0700

    test

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,7 @@ export default function Home() {
   return (
     <>
     <div>My name is Toan mac goai</div>
-    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goal.com%2Fvn%2Flist%2Fmu-harry-maguire-ga-doi-truong-dang-thuong-cua-quy-do%2Fblt0a64997bdce30bc4&psig=AOvVaw01_-DhKb40PYxXvpAcPK4U&ust=1669799293458000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKCup6CF0_sCFQAAAAAdAAAAABAD">
-    </img>
+    <div>acnj</div>
     </>
   )
 }
